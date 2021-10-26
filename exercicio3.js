796ee251a8a64e3c0ffbd05f6daebba8b47bb197
unknown <moisesbollela1998@gmail.com>
2021-10-26 19:14:40 -0300

    melhor exercicio

---
+++ exercicio3.js
@@ -0,0 +1,14 @@
+let x = 1;
+let y = 2;
+let z = 3;
+
+let soma = x + y;
+let sub = x-y;
+
+console.log(3+5);
+console.log(soma);
+console.log(sub);
+console.log("1"+1);
+console.log("olá" + 23);
+console.log("olá, eu tenho "+ x +" anos");
+
