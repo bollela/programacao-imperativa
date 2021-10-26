ce8a3710a9b7483537488fe09f3304f5656d4a58
unknown <moisesbollela1998@gmail.com>
2021-10-26 19:16:24 -0300

    melhor exercicio

---
+++ exercicio4.js
@@ -0,0 +1,39 @@
+//EXERCÍCIO EM MESA - AULA 04
+//Fabrina Nunes, Cristiano Dewes, Moises Bollela, Guilherme Pereira e Allison Souza;
+
+//soma dois números
+console.log(2+4)
+
+//outro operador
+console.log(2*4)
+
+//atribuir 2 variáveis
+let num1 = 3;
+let num2 = 4;
+
+console.log(num1+num2);
+
+//atribuir a soma a uma variável
+let soma = num1+num2;
+console.log(soma);
+
+//reatribuir
+soma = "1"+1;
+console.log(soma);
+
+//reatribuir
+soma = "olá"+23;
+console.log(soma);
+
+//concatenar
+console.log('O valor do número 1 é', num1);
+
+///////////////////////////////////////////////// EXERCÍCIO 02
+
+let nome = 'José da Silva';
+let idade = 27;
+let peso = 83.5;
+let altura = 1.7;
+let plano = true;
+let IMC = peso/(altura ^ 2);
+console.log(nome, 'tem', idade, 'anos. O seu IMC é de', IMC);
